a6666398500b59a290e814f63f1eedffe86a7d00
Davide <davelorino@gmail.com>
2020-05-18 20:56:13 +1000

    banking sector branded keywords table + app logos 2

--- app.R
+++ app.R
@@ -88,6 +88,7 @@ pdf(NULL)
     #uris <- readRDS("uris.rds")
     #urisclick <- readRDS("urisclick.rds")
     destinations_test_data <- readRDS("destinations_test.rds")
+    kw_branded_top25 <- readRDS("kw_branded_top25.rds")
   
     #coronavirus <- readRDS("coronavirus2.rds")
     corona7daycases <- readRDS("corona7daycases.rds")
@@ -847,11 +848,7 @@ pdf(NULL)
                   do.call(span, args)
                 }
                 spotify_theme <- function() {
-                  search_icon <- function(fill = "none") {
-                    # Icon from https://boxicons.com
-                    svg <- sprintf('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="%s" d="M10 18c1.85 0 3.54-.64 4.9-1.69l4.4 4.4 1.4-1.42-4.39-4.4A8 8 0 102 10a8 8 0 008 8.01zm0-14a6 6 0 11-.01 12.01A6 6 0 0110 4z"/></svg>', fill)
-                    sprintf("url('data:image/svg+xml;base64,%s')", jsonlite::base64_enc(svg))
-                  }
+                
                   text_color <- "hsl(0, 0%, 95%)"
                   text_color_light <- "hsl(0, 0%, 70%)"
                   text_color_lighter <- "hsl(0, 0%, 55%)"
@@ -909,7 +906,6 @@ pdf(NULL)
                       backgroundPosition = "left 8px center",
                       backgroundRepeat = "no-repeat",
                       "&:focus" = list(backgroundColor = "rgba(255, 255, 255, 0.1)", border = "none"),
-                      "&:hover, &:focus" = list(backgroundImage = search_icon(text_color)),
                       "::placeholder" = list(color = text_color_lighter),
                       "&:hover::placeholder, &:focus::placeholder" = list(color = text_color)
                     ),
@@ -918,10 +914,7 @@ pdf(NULL)
                     pageButtonActiveStyle = list(backgroundColor = "hsl(0, 0%, 24%)")
                   )
                 }
-                
-              
-                    tracks_table(data)
-                
+                 tracks_table(data)
               }
               
               branded_table <- renderKeywordsBankingSectorTable(kw_branded_top25)
